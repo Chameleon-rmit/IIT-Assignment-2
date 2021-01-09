28b87ab850736f1c9a3930c3a47e054cb4cdd65f
Chameleon-rmit <s3884930@student.rmit.edu.au>
2021-01-09 10:07:40 +1000

    Snapshot commit for the Saturday meeting, still in the progress transferring content from the content documents to the PWA

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+self.addEventListener('install', function(event)
+  {
+    event.waitUntil(
+      caches.open("full_app").then(function(cache)
+        {
+          return cache.addAll(
+            [
+              '/index.html',
+            ]);
+        });
+    );
+  });
+
+self.addEventListener('fetch', event =>
+  {
+    event.respondWith(async function()
+    {
+      const cache = await caches.open("full_app");
+      const cachedResponse = await cache.match(event.request);
+
+      if(cachedResponse)
+      {
+        event.waitUntil(cache.add(event.request));
+        return cachedResponse;
+      }
+
+      return fetch(event.request);
+    }
+  });
