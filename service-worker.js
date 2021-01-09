b08eb724a49ace3b6500d2401050ed577790ebc9
Chameleon-rmit <s3884930@student.rmit.edu.au>
2021-01-09 12:25:19 +1000

    ..

--- service-worker.js
+++ service-worker.js
@@ -15,11 +15,9 @@ self.addEventListener('fetch', event =>
   {
     event.respondWith(async function()
     {
-      return fetch(event.request);
       const cache = await caches.open("full_app");
       const cachedResponse = await cache.match(event.request);
 
-      console.log(cachedResponse);
       if(cachedResponse)
       {
         event.waitUntil(cache.add(event.request));
