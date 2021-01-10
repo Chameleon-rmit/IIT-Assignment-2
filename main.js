1daac089eb651404d19b1c47847c71d8569b6d07
Chameleon-rmit <s3884930@student.rmit.edu.au>
2021-01-10 11:47:12 +1000

    Major refactor to split out the content, style, shell, and logic into their own files

---
+++ main.js
@@ -0,0 +1,186 @@
+pages = [
+        { 'title': 'Introduction',
+        },
+        { 'title': 'Team Profile',
+        },
+        { 'title': 'Ideal Jobs',
+        },
+        { 'title': 'Tools',
+        },
+        { 'title': 'Industry Data',
+        },
+        { 'title': 'IT Work',
+        },
+        { 'title': 'IT Technologies',
+        },
+        { 'title': 'Project Idea',
+        },
+        { 'title': 'Group Reflection',
+        },
+        { 'title': 'Bibliography',
+        },
+        ];
+
+customElements.define('nav-home', class NavHome extends HTMLElement
+  {
+    connectedCallback()
+    {
+      this.innerHTML = `
+        <ion-header translucent>
+          <ion-toolbar>
+            <ion-title slot="start">SEEKS - I2IT A2</ion-title>
+            <ion-item lines="none">
+              <ion-icon name="moon" slot="end"></ion-icon>
+              <ion-toggle id="themeToggle" slot="end"></ion-toggle>
+            </ion-item>
+          </ion-toolbar>
+        </ion-header>
+        <ion-content fullscreen>
+          <ion-list>
+          ${pages.map(page => `
+            <ion-item button onclick="showDetail('${page.title}')">
+              <ion-label>
+                <h3>${page.title}</h3>
+              </ion-label>
+            </ion-item>
+          `).join('\n')}
+          </ion-list>
+        </ion-content>
+      `;
+
+    const toggle = document.querySelector('#themeToggle');
+    if(window.localStorage.darkMode=="true")
+    {
+      toggle.checked=true;
+      document.body.classList.toggle('dark', true);
+    }
+    toggle.addEventListener('ionChange', (ev) =>
+      {
+        document.body.classList.toggle('dark', ev.detail.checked);
+        window.localStorage.darkMode=ev.detail.checked;
+      });
+
+    }
+  });
+
+const nav = document.querySelector('ion-nav');
+
+function showDetail(title)
+{
+  const page = pages.find(page => page.title === title);
+  nav.push('nav-detail', { page });
+}
+
+customElements.define('nav-detail', class NavDetail extends HTMLElement
+  {
+    connectedCallback()
+    {
+      var object = this.page ? this.page:this.tech;
+      this.innerHTML = `
+        <ion-header translucent>
+          <ion-toolbar>
+            <ion-buttons slot="start">
+              <ion-back-button defaultHref="/"></ion-back-button>
+            </ion-buttons>
+            <ion-title>${object.title}</ion-title>
+          </ion-toolbar>
+        </ion-header>
+        <ion-content id="${object.title}" fullscreen class="ion-padding">
+        </ion-content>
+        `;
+      get_content(object.title);
+    }
+  });
+
+function status(response)
+{
+  if(response.status >= 200 && response.status < 300)
+  {
+    return Promise.resolve(response);
+  }
+  else
+  {
+    return Promise.reject(new Error(response.statusText));
+  }
+}
+
+function get_content(title)
+{
+  const content=document.getElementById(title);
+  fetch('./content/'+title.replace(/\\/g, ' ')+".html")
+  .then(status)
+  .then(function(response)
+    {
+      return response.text();
+    }) 
+  .then(function(html)
+    {
+      content.innerHTML=html;
+    })
+  .catch(function(error)
+    {
+      content.innerHTML="Ack, we got an error for content `"+title+"`: "+error;
+    });
+}
+
+const technologies = [
+  { 'title': 'Machine Learning',
+  },
+  { 'title': 'Virtual/Augmented/Extended Reality',
+  },
+  { 'title': 'Natural Language Processing',
+  },
+  { 'title': 'Blockchain',
+  },
+];
+
+function it_technologies_content_function(page)
+{
+  return `
+        <ion-content fullscreen>
+          <ion-list>
+          ${technologies.map(tech => `
+            <ion-item button onclick="showTech('${tech.title}')">
+              <ion-label>
+                <h3>${tech.title}</h3>
+              </ion-label>
+            </ion-item>
+          `).join('\n')}
+          </ion-list>
+        </ion-content>
+      `;
+}
+
+function showTech(title)
+{
+  const tech = technologies.find(tech => tech.title == title);
+  nav.push('nav-detail', { tech });
+}
+
+customElements.define('nav-detail-tech', class NavDetail extends HTMLElement
+  {
+    connectedCallback()
+    {
+      this.innerHTML = `
+      <ion-header translucent>
+        <ion-toolbar>
+          <ion-buttons slot="start">
+            <ion-back-button defaultHref="/"></ion-back-button>
+          </ion-buttons>
+          <ion-title>${this.tech.title}</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content id="${object.title}" fullscreen class="ion-padding">
+      </ion-content>
+      `;
+      get_content(this.tech.title);
+    }
+  });
+
+if('serviceWorker' in navigator)
+{
+  window.addEventListener('load', function()
+    {
+      navigator.serviceWorker.register('./service-worker.js');
+    });
+}
--- service-worker.js
+++ service-worker.js
@@ -11,27 +11,31 @@ self.addEventListener('install', function(event)
     );
   });
 
-// .. Okay, this could use a little explanation
+// .. Okay, this could use a little explanation, especially as we're using Promises
 // When our web app asks for something from a link...
 self.addEventListener('fetch', event =>
   {
-    event.respondWith(async function()
-    {
-      // ... check if it's in our cache...
-      const cachedResponse = await caches.match(event.request);
-
-      // ... and if it is, return it...
-      if(cachedResponse)
-        return cachedResponse;
-
-      // ... if it isn't then fetch the response into a variable...
-      const response = await fetch(event.request);
-      const cache = await caches.open("full_app");
-      // ... so that we can put a copy of it in the cache...
-      cache.put(event.request, response.clone());
-      // ... as well as returning the response to our application. ...
-      return response;
-    }())
+    event.respondWith(
+    caches.open("full_app")
+      .then(cache =>
+        {
+          // ... look up the request in the cache ...
+          return cache.match(event.request)
+            .then(offline =>
+              {
+                // ... even if we have a cached value fetch it from the network for the newest version ...
+                var online = fetch(event.request)
+                  .then(networkResponse =>
+                    {
+                      // ... and put it in our cache ...
+                      cache.put(event.request, networkResponse.clone());
+                      return networkResponse;
+                    });
+                // ... return the online version if we can, otherwise the cached version ...
+                return online || offline;
+              });
+        }),
+    );
   });
 // ... Some implications of this is that it will cache the whole application.
 // Which is fine because it's going to be entirely static anyway. Something to keep in mind though.
